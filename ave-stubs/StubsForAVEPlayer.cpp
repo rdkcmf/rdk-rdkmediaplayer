ec27d3a435d38e8f39eb146e37641989948e0115
Vinish K B <Vinish_KorasseryBalakrishnan@comcast.com>
2019-01-16 19:07:05 +0000

    RDK-22205: fix compilation errors when linking with AVE libs
    Reason for change: Add stubs for symbols that throws undefined reference
    error while linking to libAVEPlayer.so
    Test Procedure: None
    Risks:Low
    Signed-off-by: Vinish K B <Vinish_KorasseryBalakrishnan@comcast.com>
    
    (cherry picked from commit d2130f5e8cb107ef8b1d86f8e02f863b91f023c8)
    (cherry picked from commit 7789c3dd98c71b4924efcad3f169d29f2abd9121)
    
    Change-Id: I26fde1c47f60d59083ef82b517d650b8590e258b

---
+++ ave-stubs/StubsForAVEPlayer.cpp
@@ -0,0 +1,78 @@
+/*
+ * If not stated otherwise in this file or this component's Licenses.txt file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2018 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+/**
+ * @file StubsForAVEPlayer.cpp
+ * @brief Various stubs required to compile AVE drm
+ * @note To be removed once avedrm is used instead of aveplayer
+ */
+
+/*Stubs to link with libAVEPlayer.so*/
+extern "C" {
+
+void JSValueToObject(){ }
+void JSStringCreateWithUTF8CString(){ }
+void JSStringGetCharactersPtr(){ }
+void JSValueMakeNull(){ }
+void JSValueIsUndefined(){ }
+void JSClassRelease(){ }
+void JSValueIsObjectOfClass(){ }
+void JSStringRelease(){ }
+void JSObjectSetProperty(){ }
+void JSValueMakeNumber(){ }
+void JSValueToBoolean(){ }
+void JSObjectIsFunction(){ }
+void JSValueIsBoolean(){ }
+void JSValueToStringCopy(){ }
+void JSValueToNumber(){ }
+void JSValueIsString(){ }
+void JSValueUnprotect(){ }
+void JSObjectSetPrivate(){ }
+void JSStringGetUTF8CString(){ }
+void JSContextGetGlobalObject(){ }
+void JSValueMakeString(){ }
+void JSGarbageCollect(){ }
+void JSStringGetLength(){ }
+void JSObjectMakeConstructor(){ }
+void JSObjectCallAsFunction(){ }
+void JSValueMakeBoolean(){ }
+void JSValueIsObject(){ }
+void JSObjectGetPropertyAtIndex(){ }
+void JSObjectMakeFunction(){ }
+void JSObjectCopyPropertyNames(){ }
+void JSPropertyNameArrayRelease() { }
+void JSObjectIsConstructor(){ }
+void JSStringGetMaximumUTF8CStringSize(){ }
+void JSValueMakeUndefined(){ }
+void JSObjectMake(){ }
+void JSPropertyNameArrayGetNameAtIndex(){ }
+void JSValueIsInstanceOfConstructor(){ }
+void JSContextGetGlobalContext(){ }
+void JSValueIsNumber(){ }
+void JSObjectHasProperty(){ }
+void JSObjectGetPrivate(){ }
+void JSPropertyNameArrayGetCount(){ }
+void JSObjectMakeArray(){ }
+void JSObjectGetProperty(){ }
+void JSClassCreate(){ }
+void JSValueProtect(){ }
+void JSStringCreateWithCharacters(){ }
+void JSObjectMakeDate(){ }
+void JSObjectMakeError(){ }
+}
